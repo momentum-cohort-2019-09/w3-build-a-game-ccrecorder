f6f93636f2110f6cd0dee2ce9b355a52188b81a4
Charles Wines <ccrecorder@gmail.com>
2019-10-01 00:15:08 -0400

    both doody and map! but no movement :(

--- classGame.js
+++ classGame.js
@@ -8,8 +8,6 @@ class Game {
 		this.gameOver = false;
 		this.bodies = [];
 
-		const img = document.getElementById('imageId').onload;
-
 		let doodySize = {
 			width  : 36,
 			height : 36
@@ -17,7 +15,7 @@ class Game {
 
 		let doodyLocation = {
 			x : 331,
-			y : 267
+			y : 339
 		};
 
 		this.doody = new Doody(doodyLocation, doodySize);
@@ -60,33 +58,33 @@ class Doody {
 	constructor(location, size) {
 		this.location = location;
 		this.size = size;
-		this.jumping = false;
 	}
 
 	update(game) {
-		if (game.keyboard.isDown(Keyboarder.KEYS.RIGHT) && !this.jumping) {
+		if (game.keyboard.isDown(Keyboarder.KEYS.RIGHT)) {
 			this.jumping = true;
-			this.doodyLocation.x += 36;
+			this.location.x += 36;
 		}
-		if (game.keyboard.isDown(Keyboarder.KEYS.LEFT) && !this.jumping) {
+		if (game.keyboard.isDown(Keyboarder.KEYS.LEFT)) {
 			this.jumping = true;
-			this.doodyLocation.x -= 36;
+			this.location.x -= 36;
 		}
-		if (game.keyboard.isDown(Keyboarder.KEYS.UP) && !this.jumping) {
+		if (game.keyboard.isDown(Keyboarder.KEYS.UP)) {
 			this.jumping = true;
-			this.doodyLocation.y -= 36;
+			this.location.y -= 36;
 		}
-		if (game.keyboard.isDown(Keyboarder.KEYS.DOWN) && !this.jumping) {
+		if (game.keyboard.isDown(Keyboarder.KEYS.DOWN)) {
 			this.jumping = true;
-			this.doodyLocation.y += 36;
+			this.location.y += 36;
 		}
 	}
 
 	draw(screen) {
-		screen.fillStyle = '#3D2D17';
-		screen.fillRect(331, 267, 36, 36);
-		screen.strokeRect(331, 267, 36, 36);
-		// screen.drawImage(img, this.location.x, this.location.y, this.size.x, this.size.y);
+		// screen.fillStyle = '#3D2D17';
+		// screen.fillRect(331, 339, 36, 36);
+		// screen.strokeRect(331, 339, 36, 36);
+		const img = document.getElementById('imageId');
+		screen.drawImage(img, this.location.x, this.location.y, this.size.width, this.size.height);
 	}
 }
 
@@ -97,17 +95,19 @@ class Square {
 		this.z = z;
 	}
 
-	update() {}
+	update() {
+		console.log(bodies);
+	}
 
 	draw(screen) {
-		if (square.z === 1) {
+		if (this.z === 1) {
 			screen.fillStyle = '#FFFFFF';
 		}
-		if (square.z === 2) {
+		if (this.z === 2) {
 			screen.fillStyle = '#333333';
 		}
-		screen.fillRect(square.x, square.y, 36, 36);
-		screen.strokeRect(square.x, square.y, 36, 36);
+		screen.fillRect(this.x, this.y, 36, 36);
+		screen.strokeRect(this.x, this.y, 36, 36);
 	}
 }
 
